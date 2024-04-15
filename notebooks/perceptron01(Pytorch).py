714b1b0a2b7a9d3a911719a2b3b1a20fb9ac5d86
Hepheir <hepheir@gmail.com>
2024-04-16 02:51:24 +0900

    refactor(w02): nn library 사용

--- notebooks/perceptron01(Pytorch).py
+++ notebooks/perceptron01(Pytorch).py
@@ -8,25 +8,28 @@ N, Din, Dout = 64, 4, 2
 
 x = torch.randn(N, Din, device=device)
 y = torch.randn(N, Dout, device=device)
-w = torch.randn(Din, Dout, device=device, requires_grad=True)
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(Din, Dout),
+)
 
 lr = 1e-6
 loss_arr = []
 
 for t in range(50000):
     # 5-1. 예측 값을 계산
-    y_hat = x.mm(w)
+    y_hat = model(x)
 
     # 5-2. loss 계산
-    loss = (0.5 * (y_hat - y) ** 2).sum()
+    loss = torch.nn.functional.mse_loss(y_hat, y)
     loss_arr.append(loss)
 
     # 5-3. 경사하강법 적용
     loss.backward()
 
     with torch.no_grad():
-        w -= lr * w.grad
-        w.grad.zero_()
+        for param in model.parameters():
+            param -= lr * param.grad
+        model.zero_grad()
 
-
-plt.plot([loss.detach() for loss in loss_arr])
+plt.plot([loss.cpu().detach() for loss in loss_arr])
