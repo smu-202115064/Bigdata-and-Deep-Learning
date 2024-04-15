97d13abbb6f6df2dade37be12c8b13320d825d59
Hepheir <hepheir@gmail.com>
2024-04-16 02:04:58 +0900

    refactor(w02): Perceptron 실습 (Pytorch) 적용

--- notebooks/perceptron01(Pytorch).py
+++ notebooks/perceptron01(Pytorch).py
@@ -1,19 +1,21 @@
-import numpy as np
+import torch
 import matplotlib.pyplot as plt
 
 
+device = torch.device('cpu')
+
 N, Din, Dout = 64, 4, 2
 
-x = np.random.randn(N, Din)
-y = np.random.randn(N, Dout)
-w = np.random.randn(Din, Dout)
+x = torch.randn(N, Din, device=device)
+y = torch.randn(N, Dout, device=device)
+w = torch.randn(Din, Dout, device=device)
 
 lr = 1e-6
 loss_arr = []
 
 for t in range(50000):
     # 5-1. 예측 값을 계산
-    y_hat = np.matmul(x, w)
+    y_hat = x.mm(w)
 
     # 5-2. loss 계산
     loss = (0.5 * (y_hat - y) ** 2).sum()
@@ -21,7 +23,7 @@ for t in range(50000):
 
     # 5-3. 경사하강법 적용
     grad_y_hat = y_hat - y
-    grad_w = np.matmul(x.T, grad_y_hat)
+    grad_w = x.T.mm(grad_y_hat)
 
     # 5-4. w를 갱신
     w -= lr * grad_w
